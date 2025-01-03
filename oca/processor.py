0bfa7a38853aaae0298013df2b08ab355b93517d
pstlouis <patrick.st-louis@opsecid.ca>
2025-01-03 17:41:02 -0500

    linting
    Signed-off-by: pstlouis <patrick.st-louis@opsecid.ca>

--- oca/processor.py
+++ oca/processor.py
@@ -24,11 +24,11 @@ class OCAProcessor:
 
     def secure_bundle(self, bundle):
         capture_base = bundle[0]
-        overlays = capture_base.pop('overlays')
-        
+        overlays = capture_base.pop("overlays")
+
         capture_base["digest"] = self.dummy_string
         capture_base["digest"] = self.generate_said(capture_base)
-        
+
         for idx, overlay in enumerate(overlays):
             overlays[idx]["capture_base"] = capture_base["digest"]
             overlays[idx]["digest"] = self.dummy_string
@@ -40,42 +40,49 @@ class OCAProcessor:
     def draft_bundle(self, schema):
         capture_base = {
             "type": "spec/capture_base/1.0",
-            "attributes": {attribute: 'Text' for attribute in schema['attributes']},
+            "attributes": {attribute: "Text" for attribute in schema["attributes"]},
             "flagged_attributes": [],
-            'overlays': []
+            "overlays": [],
         }
         encoding = {
             "type": "spec/overlays/character_encoding/1.0",
             "default_character_encoding": "utf-8",
-            "attribute_character_encoding": {attribute: "utf-8" for attribute in schema['attributes']},
+            "attribute_character_encoding": {
+                attribute: "utf-8" for attribute in schema["attributes"]
+            },
         }
-        capture_base['overlays'].append(encoding)
-        
+        capture_base["overlays"].append(encoding)
+
         labels = {
             "type": "spec/overlays/label/1.0",
             "lang": "en",
-            "attribute_labels": {attribute: attribute.replace('_', ' ').capitalize() for attribute in schema['attributes']},
+            "attribute_labels": {
+                attribute: attribute.replace("_", " ").capitalize()
+                for attribute in schema["attributes"]
+            },
         }
-        capture_base['overlays'].append(labels)
-        
+        capture_base["overlays"].append(labels)
+
         information = {
             "type": "spec/overlays/information/1.0",
             "lang": "en",
-            "attribute_information": {attribute: 'Lorem ipsum' for attribute in schema['attributes']},
+            "attribute_information": {
+                attribute: "Lorem ipsum" for attribute in schema["attributes"]
+            },
         }
-        capture_base['overlays'].append(information)
-        
+        capture_base["overlays"].append(information)
+
         meta = {
             "type": "spec/overlays/meta/1.0",
             "language": "en",
             "issuer": schema["name"],
             "name": schema["name"],
-            "description": schema['description'],
+            "description": schema["description"],
             "credential_help_text": "Learn more",
-            "credential_support_url": ""
+            "credential_support_url": "",
         }
-        capture_base['overlays'].append(meta)
-        
+        capture_base["overlays"].append(meta)
+
         branding = {
             "type": "aries/overlays/branding/1.0",
             "logo": "",
@@ -87,7 +94,7 @@ class OCAProcessor:
             "secondary_attribute": "",
             "expiry_date_attribute": "",
         }
-        capture_base['overlays'].append(branding)
+        capture_base["overlays"].append(branding)
         return [capture_base]
 
     def create_bundle(self, credential_registration, credential_template):
